53ad6a93329d866a7558b0f058df364d2d9a0107
Raj Bhattacharyya <rajbhattacharyya18110@gmail.com>
2023-12-28 02:42:13 +0530

    added new

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <div className="relative flex flex-col items-center justify-center [background:linear-gradient(125.42deg,_rgba(202,_75,_4,_0.2),_rgba(202,_75,_4,_0)),_#0f0f0f] w-full min-h-screen overflow-hidden text-center text-[1.25rem] text-white font-montserrat">
-      <div className="text-[11.25rem] tracking-[0.3em] font-light font-oswald text-transparent !bg-clip-text [background:linear-gradient(180deg,_rgba(255,_255,_255,_0.98),_rgba(255,_255,_255,_0))] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent] flex items-center justify-center w-full max-w-[50.44rem] h-[14.56rem], ml-10">
-        CURATE
-      </div>
-      <div className="text-[1.5rem] tracking-[0.02em] leading-[3.13rem] font-semibold text-transparent !bg-clip-text [background:linear-gradient(90deg,_rgba(255,_255,_255,_0.9)_0.15%,_rgba(255,_255,_255,_0.12))] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent] flex items-center justify-center w-full max-w-[71rem] h-[2.13rem] [text-shadow:0px_4px_7.2px_rgba(0,_0,_0,_0.25)]">
-        THE WORLD OF ARTIFICIAL INTELLIGENCE & OPEN SOURCE
-      </div>
+    <div className={styles.landingPage}>
+      <div className={styles.curate}>CURATE</div>
+      <div className={styles.theWorldOf}>{`THE WORLD OF ARTIFICIAL INTELLIGENCE & OPEN SOURCE`}</div>
     </div>
   );
 }

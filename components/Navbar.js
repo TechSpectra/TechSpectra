5f58b2b5eeb51c347b5dae8e111f6ca654ee7a50
Mrinmay Das <mrinmaydas2604@gmail.com>
2024-01-04 20:33:04 +0530

    navbar fixes

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,10 +15,12 @@ export default function Navbar() {
         height={120}
         priority
       />
+      <div className={styles.links}>
       <div className={styles.home1}>HOME</div>
       <div className={styles.aboutUs}>ABOUT US</div>
       <div className={styles.aboutUs}>CONTACT US</div>
       <div className={styles.aboutUs}>RESOURCES</div>
+      </div>
     </div>
   );
 }
